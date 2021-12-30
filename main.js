e4d0f7adf37c6c3be9f466f0fc410127fb0ae64c
Snehasis Ghosh <91667746+snehasisserver@users.noreply.github.com>
2021-12-30 11:28:07 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+var week = 7;
+    var day =("365   ,       " +356 +" Days "+  "in leap year");
+    var rainbow = 7;
+    var ship = "camels";
+    var not_v = ("24" + "  consonant");
+function display(){
+    
+
+    document.getElementById("ans1").innerHTML=week;
+    document.getElementById("ans2").innerHTML=day;
+    document.getElementById("ans3").innerHTML=rainbow;
+    document.getElementById("ans4").innerHTML=ship;
+    document.getElementById("ans5").innerHTML=not_v;
+   
+}
